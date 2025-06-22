c999b06ba454675de2f18b59e776a6f9cfc28a93
Maxime Bodin <maxime.bodin@mailo.com>
2025-06-22 21:57:32 +0200

    Disabling switch while loading space.

--- components/custom/pageWrapper.tsx
+++ components/custom/pageWrapper.tsx
@@ -2,17 +2,22 @@
 
 import { usePathname, useRouter } from 'next/navigation'
 import { Switch } from '@/components/ui/switch'
+import React, { useTransition } from 'react'
 
 export default function PageWrapper({ children }: { children: React.ReactNode }) {
    const router = useRouter()
    const pathname = usePathname()
 
+   const [isPending, startTransition] = useTransition()
+
    const handleSwitchChange = (checked: boolean) => {
-      if (checked && pathname !== '/space') {
-         router.push('/space')
-      } else if (!checked && pathname !== '/') {
-         router.push('/')
-      }
+      startTransition(() => {
+         if (checked && pathname !== '/space') {
+            router.push('/space')
+         } else if (!checked && pathname !== '/') {
+            router.push('/')
+         }
+      })
    }
 
    return (
@@ -21,6 +26,7 @@ export default function PageWrapper({ children }: { children: React.ReactNode })
             <Switch
                onCheckedChange={handleSwitchChange}
                checked={pathname === '/space'}
+               disabled={isPending}
                className="bg-primary"
             />
          </div>
