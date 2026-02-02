ccd94338c0190658668eabff229c4cb6299ce62d
Maxime Bodin <maxime.bodin@mailo.com>
2026-02-02 05:36:58 +0100

    Fixing carousel invisible arrows.

--- components/ui/carousel.tsx
+++ components/ui/carousel.tsx
@@ -204,10 +204,10 @@ const CarouselPrevious = React.forwardRef<
          variant={variant}
          size={size}
          className={cn(
-            'absolute  h-8 w-8 rounded-full',
+            'absolute h-8 w-8 rounded-full z-10',
             orientation === 'horizontal'
-               ? '-left-12 top-1/2 -translate-y-1/2'
-               : '-top-12 left-1/2 -translate-x-1/2 rotate-90',
+               ? '-left-4 top-1/2 -translate-y-1/2'
+               : '-top-4 left-1/2 -translate-x-1/2 rotate-90',
             className,
          )}
          disabled={!canScrollPrev}
@@ -233,10 +233,10 @@ const CarouselNext = React.forwardRef<
          variant={variant}
          size={size}
          className={cn(
-            'absolute h-8 w-8 rounded-full',
+            'absolute h-8 w-8 rounded-full z-10',
             orientation === 'horizontal'
-               ? '-right-12 top-1/2 -translate-y-1/2'
-               : '-bottom-12 left-1/2 -translate-x-1/2 rotate-90',
+               ? '-right-4 top-1/2 -translate-y-1/2'
+               : '-bottom-4 left-1/2 -translate-x-1/2 rotate-90',
             className,
          )}
          disabled={!canScrollNext}
