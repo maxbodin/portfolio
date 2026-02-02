c64897f237a3377fec7bb2fa0b07274cc848719d
Maxime Bodin <maxime.bodin@mailo.com>
2026-02-02 06:12:39 +0100

    Translating events data.

--- data/events.ts
+++ data/events.ts
@@ -1,32 +1,35 @@
-/* eslint-disable max-len */
 import { Skill } from '@/functions/getSkillColor'
 import { WorkDetails } from '@/interfaces/workDetails'
 
 export const eventsItems: WorkDetails[] = [
    {
-      description: 'Participation en équipe au #24hDeLinfo des IUT de France ! Concours composé de trois épreuves, chacune d’une durée de 8 heures. Nous avons fièrement terminé 8ème sur 34 équipes.',
-      image_path: '/images/logo2024.webp',
+      date: '2024',
+      description: 'Participated as a team in the #24hDeLinfo, a national competition for French IUTs. The contest consisted of three 8-hour challenges. Our team proudly finished 8th out of 34 teams.',
+      main_image_path: '/images/logo2024.webp',
       link: 'https://24hinfo.iut.fr',
       skills: [Skill.ALGO_PROG, Skill.DEV_WEB, Skill.SEC],
       title: '24h de l\'Info 2024',
    },
    {
-      description: 'Participation en équipe à la Nuit de l\'Info 2023 sur le campus CESI de La Rochelle. Le but de cette nuit de développement était de créer un site web sur le thème du changement climatique 🌎, afin de démystifier le vrai du faux. Nous avons développer un jeu dans lequel l\'utilisateur peut gérer l\'état de sa planète via des choix.',
-      image_path: '/images/mylittleplanet.jpeg',
+      date: '2023',
+      description: 'Team participation in the Nuit de l\'Info 2023 at the CESI campus in La Rochelle. The goal of this overnight hackathon was to create a website on the theme of climate change 🌎, aiming to distinguish fact from fiction. We developed a game where the user manages the health of their planet through various choices.',
+      main_image_path: '/images/mylittleplanet.jpeg',
       link: 'https://nuit-info-2024.vercel.app',
       skills: [Skill.REACT, Skill.THREE_JS, Skill.HLSL, Skill.COM],
       title: 'Nuit de l\'info 2023',
    },
    {
-      description: 'Lors de la game jam GMTK 2O23, nous avons réalisé le jeu Island Survivors, en binôme.',
-      image_path: '/images/island_survivors.png',
+      date: '2023',
+      description: 'During the GMTK Game Jam 2023, we created the game "Island Survivors" as a team of two.',
+      main_image_path: '/images/island_survivors.png',
       link: 'https://makzime.itch.io/island-survivors-chaos-upgrade',
       skills: [Skill.UNITY, Skill.C_SHARP, Skill.GITLAB, Skill.GESTION_PROJ, Skill.GIMP, Skill.PAINT_NET],
-      title: 'Game Jam GMTK 2023',
+      title: 'GMTK Game Jam 2023',
    },
    {
-      description: 'Participation en équipe à la Nuit de l\'Info 2022 sur le campus CESI de La Rochelle. Le but de cette nuit de développement était de créer un site web permettant de favoriser l’accès à une information pertinente et adaptée sur la santé sexuelle. Mon rôle a été de développé un serious game au sein de notre site. Nous avons obtenu la première place au défi "Easter egg", proposé par l\'entreprise Code-Troopers.',
-      image_path: '/images/n2i_2022.jpg',
+      date: '2022',
+      description: 'Participated as a team in the Nuit de l\'Info 2022 at the CESI campus in La Rochelle. The goal was to create a website promoting access to relevant and appropriate information on sexual health. My role was to develop a serious game within our site. We won first place in the "Easter Egg" challenge, sponsored by the company Code-Troopers.',
+      main_image_path: '/images/n2i_2022.jpg',
       link: 'https://www.nuitdelinfo.com',
       skills: [Skill.HTML, Skill.CSS, Skill.COM],
       title: 'Nuit de l\'info 2022',
