bc651a0cbcfbf5d5f1ee86b565ca201062d209bb
Maxime Bodin <maxime.bodin@mailo.com>
2026-02-02 05:06:14 +0100

    Removing unused file.

--- components/custom/sections/goals.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react'
-
-export default function Goals() {
-   return (<section id="goals" className="pt-16">
-      <h2 className="text-2xl pt-8 pb-4">Objectifs 🎯</h2>
-      <p>Work In Progress...</p>
-   </section>)
-}
