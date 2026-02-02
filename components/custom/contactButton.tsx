b24fc9ed2828896d27fb054baca631af6b551b7d
Maxime Bodin <maxime.bodin@mailo.com>
2026-02-02 02:41:08 +0100

    Translate contact button.

--- components/custom/contactButton.tsx
+++ components/custom/contactButton.tsx
@@ -3,11 +3,12 @@ import Link from 'next/link'
 import { Mail } from 'lucide-react'
 
 export default function ContactButton() {
-   return (<div className="flex flex-col items-center pt-20">
-      <Button variant="outline" asChild>
-         <Link href="mailto:maxbod.contact@gmail.com">
-            <Mail className="mr-2 h-4 w-4" />Me contacter !
-         </Link>
-      </Button>
-   </div>)
+   return (
+      <div className="flex flex-col items-center pt-20">
+         <Button variant="outline" asChild>
+            <Link href="mailto:maxbod.contact@gmail.com">
+               <Mail className="mr-2 h-4 w-4" />Contact me!
+            </Link>
+         </Button>
+      </div>)
 }
