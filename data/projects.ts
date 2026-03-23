75afd1b8a8a9c84f011c6e9ae8d661512a87150a
Maxime Bodin <maxime.bodin@mailo.com>
2026-03-23 04:08:28 +0100

    Updating works path.

--- data/projects.ts
+++ data/projects.ts
@@ -104,7 +104,7 @@ export const projectsItems: WorkDetails[] = [
    },
    {
       description: 'Team project carried out as part of my 3rd year of a BUT in Computer Science at the IUT of La Rochelle. Design and implementation of an innovative application to help visit the heritage of La Rochelle. As the team\'s lead developer, I had the opportunity to guide my colleagues in adopting good development practices, thus contributing to the production of a robust and sustainable application.',
-      main_image_path: '/videos/visite_patrimoine.webm',
+      main_image_path: '/videos/opti/projects/visite_patrimoine.webm',
       skills: [Skill.FLUTTER, Skill.SYMFONY, Skill.API_PLATFORM, Skill.REST_API, Skill.ORS, Skill.GESTION_PROJ],
       title: 'Visite Patrimoine',
       date: '2024',
@@ -140,7 +140,7 @@ export const projectsItems: WorkDetails[] = [
    },
    {
       description: 'Update of the game Island Survivors, initially created during the GMTK 2023 Game Jam.',
-      main_image_path: '/videos/island_survivors_v2.webm',
+      main_image_path: '/videos/opti/projects/island_survivors_v2.webm',
       link: 'https://makzime.itch.io/island-survivors-chaos-upgrade',
       skills: [Skill.UNITY, Skill.C_SHARP, Skill.GITLAB, Skill.GESTION_PROJ, Skill.GAME_DESIGN, Skill.GIMP, Skill.PAINT_NET],
       title: 'Island Survivors V2',
@@ -148,7 +148,7 @@ export const projectsItems: WorkDetails[] = [
    },
    {
       description: 'Creation of an acquisition system composed of an ESP32 connected to temperature, humidity, and CO2 sensors. Design and development of the program to retrieve data from the sensors and send this data to an API, in C++. Creation of a React web application serving as a diagnostic tool for the air quality sensors installed within the IUT of La Rochelle. The application allows users to consult the temperatures of the rooms and receive eco-advice based on the air quality, but also allows technicians to configure the acquisition and sending of data from an interface. Optimization of the system\'s consumption.',
-      main_image_path: '/videos/smart_campus_iot.webm',
+      main_image_path: '/videos/opti/projects/smart_campus_iot.webm',
       skills: [Skill.HTML, Skill.PHP, Skill.SYMFONY, Skill.TAILWIND, Skill.CPP, Skill.TRAV_EQUIP, Skill.GIT, Skill.AGILE, Skill.DOCKER, Skill.SQL, Skill.PHPUNIT],
       title: 'IOT Smart Campus',
       date: '2023',
@@ -164,7 +164,7 @@ export const projectsItems: WorkDetails[] = [
    },
    {
       description: 'Team design and creation of a game inspired by Street Fighter 2, developed in C++ with the Qt graphics framework. Among other things, I designed a hitbox system and a key customization system.',
-      main_image_path: '/videos/iutfighter.webm',
+      main_image_path: '/videos/opti/projects/iutfighter.webm',
       skills: [Skill.TRAV_EQUIP, Skill.GAME_DESIGN, Skill.CPP, Skill.QT, Skill.POO, Skill.GESTION_PROJ, Skill.GIT, Skill.KANBAN],
       title: 'IUT FIGHTER',
       date: '2022',
